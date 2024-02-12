0835930c3124f74e38a616e4a89b265c3ed36de7
farazali-110 <151817304+farazali-110@users.noreply.github.com>
2024-02-12 20:13:57 +0500

    Add files via upload

---
+++ logical6.py
@@ -0,0 +1,11 @@
+grade = int(input("Enter a grade: "))
+if 90 <= grade < 100:
+    print("A")  
+elif 80 <= grade < 90:
+    print("B")
+elif 70 <= grade < 80:
+    print("C")   
+elif 60 <= grade < 70:
+    print("D")   
+elif 50 <= grade < 60:
+    print("F")
